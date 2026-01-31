6aa901fbc7018cef8481d0640bbf130832b7dfd0
lukas <S10268147@connect.np.edu.sg>
2026-01-31 16:53:08 +0800

    Update config.js

--- S3 static webpage/js/config.js
+++ S3 static webpage/js/config.js
@@ -1,9 +1,10 @@
 // API Configuration
 // Replace this placeholder with your actual API Gateway URL
-const API_BASE_URL = 'https://ev2ew7elue.execute-api.us-east-1.amazonaws.com/prod';
+const API_BASE_URL = '<INSERT ACTUAL API GATEWAY URL LINK>';
 
 // API Endpoints
 const API_ENDPOINTS = {
     records: `${API_BASE_URL}/products`,
     uploads: `${API_BASE_URL}/products`
+
 };
